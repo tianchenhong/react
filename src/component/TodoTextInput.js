627d54ba690964322d00b6bd5b170f160558b76e
tianchenhong <1035939037@qq.com>
2018-05-23 18:26:08 +0800

    react

---
+++ src/component/TodoTextInput.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import classnames from 'classnames'
+
+ class TodoTextInput extends Component {
+
+  handleSubmit = e => {
+    const text = e.target.value.trim()
+    if (e.which === 13) {
+      this.props.onSave(text)
+    }
+  }
+
+  render() {
+    return (
+      <input className='new-todo'
+             type="text"
+             placeholder="What needs to be done?"
+             autoFocus="true"
+             onKeyDown={this.handleSubmit} />
+    )
+  }
+}
+export default TodoTextInput;
